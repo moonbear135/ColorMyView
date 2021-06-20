fa2bf57f9cba13019ad036e600d5ff5db28e0602
Moon <redmoon11230@gmail.com>
2021-06-21 05:52:36 +0900

    Udacity 챕터2, 2장 30절
    Exercise: Add Baseline Constraint and Button Chain
    
    앞서서 plugin, import 추가 후
    동영상 강의와 똑같이
    코딩을 변경하였을 때 이상없이
    전부 다 실행이 된다.
    
    버젼에 따른 binding이 문제가 아니라
    다른 곳에서 문제가 있었다.
    
    전부다 findViewById를
    쓰지 않고도 해결되고 수업과 같이
    해결 되었다.

--- app/src/main/java/com/android/example/colormyview/MainActivity.kt
+++ app/src/main/java/com/android/example/colormyview/MainActivity.kt
@@ -21,6 +21,38 @@ class MainActivity : AppCompatActivity() {
         setListeners()
     }
 
+    private fun setListeners() {
+        var clickableViews: List<View> =
+            listOf(box_one_text, box_two_text, box_three_text, box_four_text, box_five_text,
+                constranent_layout,
+                red_button, yellow_button, green_button)
+        for(item in clickableViews) {
+            item.setOnClickListener{ makeColored(it) }
+        }
+    }
+
+    private fun makeColored(view: View) {
+        when (view.id) {
+
+            // Boxes using Color class colors for background
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            // Boxes using Android color resources for background
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            // Boxes using custon colors for background
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+
+
 //    private lateinit var binding: ActivityMainBinding
 //
 //    override fun onCreate( savedInstanceState: Bundle?) {
@@ -32,53 +64,54 @@ class MainActivity : AppCompatActivity() {
 //    }
 
 
-    private fun setListeners() {
-
-        val view1 : View = findViewById(R.id.box_one_text)
-        val view2 : View = findViewById(R.id.box_two_text)
-        val view3 : View = findViewById(R.id.box_three_text)
-        val view4 : View = findViewById(R.id.box_four_text)
-        val view5 : View = findViewById(R.id.box_five_text)
-        val view6 : View = findViewById(R.id.constranent_layout)
-        val view7 : View = findViewById(R.id.red_button)
-        val view8 : View = findViewById(R.id.yellow_button)
-        val view9 : View = findViewById(R.id.green_button)
-
-        val clickableViews : List<View> =
-            listOf(view1,view2,view3, view4, view5, view6
-            , view7, view8, view9)
-
-
-        for (item in clickableViews) {
-            item.setOnClickListener { makeColored(it) }
-        }
-
-    }
-
-    private fun makeColored(view: View){
-        when (view.id) {
-
-            //Boxes using Color Colors for background
-            //박스의 색깔을 background의 색깔을 사용해라
-            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
-
-            //Boxes using Andorid color resources for background
-            //박스의 안드로이드 색깔을 background 리소스를 사용해라
-
-            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
-            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
-            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
-
-            //Boxes using custom colors for background
-
-            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
-            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
-            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
-
-            else -> view.setBackgroundColor(Color.LTGRAY)
-
-
-        }
-    }
+//    private fun setListeners() {
+//
+//        val view1 : View = findViewById(R.id.box_one_text)
+//        val view2 : View = findViewById(R.id.box_two_text)
+//        val view3 : View = findViewById(R.id.box_three_text)
+//        val view4 : View = findViewById(R.id.box_four_text)
+//        val view5 : View = findViewById(R.id.box_five_text)
+//        val view6 : View = findViewById(R.id.constranent_layout)
+//        val view7 : View = findViewById(R.id.red_button)
+//        val view8 : View = findViewById(R.id.yellow_button)
+//        val view9 : View = findViewById(R.id.green_button)
+//
+//        val clickableViews : List<View> =
+//            listOf(view1,view2,view3, view4, view5, view6
+//            , view7, view8, view9)
+//
+//
+//        for (item in clickableViews) {
+//            item.setOnClickListener { makeColored(it) }
+//        }
+//
+//    }
+//
+//    private fun makeColored(view: View){
+//        when (view.id) {
+//
+//            //Boxes using Color Colors for background
+//            //박스의 색깔을 background의 색깔을 사용해라
+//            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+//            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+//
+//            //Boxes using Andorid color resources for background
+//            //박스의 안드로이드 색깔을 background 리소스를 사용해라
+//
+//            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+//            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+//            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+//
+//            //Boxes using custom colors for background
+//
+//            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+//            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+//            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
+//
+//            else -> view.setBackgroundColor(Color.LTGRAY)
+//
+//
+//        }
+//    }
+//}
 }
